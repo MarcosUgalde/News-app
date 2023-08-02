e60b6e0dfa3726c9518763f193a38deb2fe6083e
Marcos <marcosugaldemartinez@gmail.com>
2023-08-02 16:45:45 +0200

    feat: added insertions for data base

---
+++ backend/sql/insertion.sql
@@ -0,0 +1,89 @@
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'El País', 'https://elpais.com/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'El Mundo', 'https://www.elmundo.es/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'ABC', 'https://www.abc.es/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'La Razón', 'https://www.larazon.es/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'elDiario.es', 'https://www.eldiario.es/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'Público', 'https://www.publico.es/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'La Vanguardia', 'https://www.lavanguardia.com/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    '20 Minutos', 'https://www.20minutos.es/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'Última Hora', 'https://www.ultimahora.es/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'The Objective', 'https://theobjective.com/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'Libertad Digital', 'https://www.libertaddigital.com/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'The Economist', 'https://www.economist.com/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'The New york Times', 'https://www.nytimes.com/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'The Washington Post', 'https://www.washingtonpost.com/'
+);
+
+INSERT INTO newspapers (
+    newspapers_name, web_url
+) VALUES (
+    'BBC', 'https://www.bbc.com'
+);
