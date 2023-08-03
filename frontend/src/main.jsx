10333cd5a8c3cf60dd65b505f7863e6beb9f14da
Marcos <marcosugaldemartinez@gmail.com>
2023-08-03 14:22:15 +0200

    feat: started creation of home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Provider from './context/Provider.jsx'
 import Register from './pages/Register/index.jsx'
 import Login from './pages/Login'
+import Home from './pages/Home/index.jsx'
 import {Switch, Route, Redirect} from 'wouter'
 
 const Main = () => {
@@ -11,6 +12,7 @@ const Main = () => {
       <Switch>
         <Route path='/signup' component={Register}/>
         <Route path='/login' component={Login}/>
+        <Route path='/' component={Home}/>
         <Redirect to='/login' />
       </Switch>
     </Provider>
