33c022e5cf7250bbc575ed741b85c9264981c473
Marcos <marcosugaldemartinez@gmail.com>
2023-08-03 14:16:04 +0200

    fix: fixed export of Guard component

--- frontend/src/components/Guard/index.jsx
+++ frontend/src/components/Guard/index.jsx
@@ -3,7 +3,10 @@ import { Redirect } from 'wouter'
 
 const Guard = ({ component: Component }) => {
     const { data, isLoading} = useUser();
+
     if(isLoading) return <p>Loading</p>
 
     return data ? <Component /> : <Redirect to="/login" />
 }
+
+export default Guard
