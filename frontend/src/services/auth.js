b0e6756d5804d96ef9d718f7bfcba50692ded534
Marcos <marcosugaldemartinez@gmail.com>
2023-08-03 13:23:09 +0200

    feat: added services for auth and getting user info

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,30 @@
+export const register = (client) => async (params) => {
+  try {
+    const { data } = await client.post("/auth/signup", params);
+    console.info("signup data: ", data);
+    return data;
+  } catch (error) {
+    console.info("signup error: ", error.message);
+  }
+};
+
+export const signin = (client) => async (params) => {
+  try {
+    const { data } = await client.post("/auth/signin", params);
+    console.info(data);
+    return data;
+  } catch (error) {
+    console.info("login error: ", error.message);
+    return { success: false };
+  }
+};
+
+export const logout = (client) => async () => {
+  try {
+    const { data } = await client.post("/auth/signoput");
+    return data;
+  } catch (error) {
+    console.info("signout error: ", error.message);
+    return { success: false };
+  }
+};
