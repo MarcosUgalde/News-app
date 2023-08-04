cc3a103c2cdc7e44d613ce72139b81872fb70367
Marcos <marcosugaldemartinez@gmail.com>
2023-08-04 11:35:16 +0200

    feat: added component for displaying newspaper info

---
+++ frontend/src/components/Newspaper/index.jsx
@@ -0,0 +1,9 @@
+function NewsPaper() {
+    return(
+        <>
+            Este componente mostrará la información de la portada de los periódicos
+        </>
+    )
+}
+
+export default NewsPaper
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+//crea un comonente que puedas usar repetidamente en esta página
+//a ese componente le pasas la info de un periódico cada vez
 function Home () {
     return (
         <>
