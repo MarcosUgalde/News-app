292f99a43b64d2e51d1c798e8757e668a9921c52
Marcos <marcosugaldemartinez@gmail.com>
2023-08-04 11:44:44 +0200

    feat: initialized creation of newspaper info component

--- frontend/src/components/Newspaper/index.jsx
+++ frontend/src/components/Newspaper/index.jsx
@@ -1,4 +1,11 @@
+import { useQuery } from "react-query"
+import { searchs } from "../../services"
+
 function NewsPaper() {
+    //falta coger la url. Crear un hook para ello
+    const { data, isLoading } = useQuery(url, () => searchs.getNewspaper(url))
+    console.log(data)
+
     return(
         <>
             Este componente mostrará la información de la portada de los periódicos
