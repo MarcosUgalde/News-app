8bd441789edeb415086586c87ec4bbeef69d572b
Marcos <marcosugaldemartinez@gmail.com>
2023-08-07 23:59:44 +0200

    feat imported useUrl hook

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,6 +1,10 @@
 //crea un comonente que puedas usar repetidamente en esta página
 //a ese componente le pasas la info de un periódico cada vez
+import { useUrl } from "../../hooks/useUrl"
 function Home () {
+    const url = useUrl()
+    console.log(url)
+    
     return (
         <>
             <h1>This is home page</h1>
