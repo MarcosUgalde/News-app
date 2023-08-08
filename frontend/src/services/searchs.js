a425b604be13950bc0f591b445291123cdc509f2
Marcos <marcosugaldemartinez@gmail.com>
2023-08-08 14:33:00 +0200

    fix: fixed service for getting newspaper info

--- frontend/src/services/searchs.js
+++ frontend/src/services/searchs.js
@@ -1,11 +1,13 @@
-//falta testear
 export const newspaperInfo = (client) => async (params) => {
   try {
-    const { data } = await client.get("/:id", params);
-    console.info(data);
-    return data;
+    const { data: response } = await client.get(
+      `http://localhost:3000/searchs/${params.url}`
+    );
+    console.info(response);
+    return response;
   } catch (error) {
     console.info("Newspaper error info: ", error.message);
+    console.log(params.url);
     return { success: false };
   }
 };
