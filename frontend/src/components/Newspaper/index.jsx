cfce3be533ac0d32c2b8879641c6e6829bdbca08
Marcos <marcosugaldemartinez@gmail.com>
2023-08-08 17:14:16 +0200

    feat: rendered info of all newspapers from database

--- frontend/src/components/Newspaper/index.jsx
+++ frontend/src/components/Newspaper/index.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from "react-query"
 import { searchs } from "../../services"
+//import { useFetch } from "../../hooks"
 
 function NewsPaper(url) {
     const { data, isLoading } = useQuery(['newspaper', url], () => searchs.getNewspaper(url))
@@ -7,10 +8,12 @@ function NewsPaper(url) {
     const website = url.url
     console.log('Website: ', website)
 
+    //const text = useFetch(website)
     if(isLoading) return <div>Loading...</div>
     return(
         <>
             <h3>{data.data[0]?.newspapers_name}</h3>
+            
         </>
     )
 }
