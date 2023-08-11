8f43a4c5d489aa1a9e8ac8ebb13494fb7c4091fb
Marcos <marcosugaldemartinez@gmail.com>
2023-08-12 00:46:53 +0200

    feat: added search component to each newspaper without functionality

--- frontend/src/components/Frontpageview/index.jsx
+++ frontend/src/components/Frontpageview/index.jsx
@@ -1,12 +1,17 @@
+import Styled from './styles'
+import SearchComponent from "../Search"
+
 function Frontpageview({ url }) { 
     console.log('url que llega al Frontpagevie: ', url)
+    
 
     return (
-        <>
+        <Styled.Body>
+            <SearchComponent />
             <div>
                 <iframe src={url} title='External site' width='500px' height='300px'></iframe>                
             </div>
-        </>
+        </Styled.Body>
     )
 }
 
--- frontend/src/components/Frontpageview/styles.jsx
+++ frontend/src/components/Frontpageview/styles.jsx
@@ -1,8 +1,15 @@
 import { styled } from "styled-components";
 
+const Body = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
 const Frame = styled.section`
     width: 500px;
 `
 export default {
+    Body,
     Frame,
 }
