308b953276b4e4e965236a4ac2f74a4fd746573c
Marcos <marcosugaldemartinez@gmail.com>
2023-08-13 18:10:24 +0200

    fix: fixed code

--- frontend/src/components/Search/index.jsx
+++ frontend/src/components/Search/index.jsx
@@ -1,9 +1,13 @@
 import { useSearch } from "../SearchContext"
 import { searchs } from "../../services";
 import { useMutation } from "react-query";
+import { useState } from "react";
 
 function SearchComponent() {
-    
+    const [payload, setPayload] = useState()
+    const { mutate } = useMutation(() => searchs.addSearch({ payload }, {onSuccess: (data) => {
+        console.log(data)
+    }}))
 
     const { searchTerm, setSearchTerm} = useSearch();
 
